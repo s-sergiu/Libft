19e4c7afaba1898d91a7fd4d3ef5bd4a5832ba87
Sergiu Ster <ssergiu@student.42heilbronn.de>
2025-05-03 20:35:50 +0200

    Improved memcpy
        *now copies size_t bytes if large enough and then byte by byte;

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -1,14 +1,46 @@
-
 #include "libft.h"
+
+void	parse_words(size_t n, size_t* dst, const size_t* src)
+{
+	const size_t*	src_size_t;
+	size_t*			dst_size_t;
+	int				word;
+	int				i;
+
+	i = 0;
+	word = n / sizeof(size_t);	
+	src_size_t = src;
+	dst_size_t = dst;
+	while (i < word)
+	{
+		dst_size_t[i] = src_size_t[i];
+		i++;
+	}
+}
+
+void	parse_rest(size_t n, char* dst, const char* src)
+{
+	char*		dst_rest;
+	const char*	src_rest;
+	int			rest;
+	int			i;
+
+	i = 0;
+	rest = n % sizeof(size_t);
+	src_rest = src;
+	dst_rest = dst;
+	while (i < rest)
+	{
+		dst_rest[i] = src_rest[i];
+		i++;
+	}
+}
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int	i;
-
 	if (!dst && !src)
 		return (NULL);
-	i = -1;
-	while (++i < (int)n)
-		((char *)dst)[i] = ((char *)src)[i];
+	parse_words(n, dst, src);
+	parse_rest(n, dst, src);
 	return (dst);
 }
